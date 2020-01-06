c3cc8281826322bf46f36f54a4fec1030db3d196
Alex Grigorenko <agrigorenko@mail.greenriver.edu>
2020-01-06 14:42:39 -0500

    Created index php header and error reporting html h1

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    /** Created by PhpStorm
+     * Alex Grigorenko
+     * IT 328
+     * 1/6/20
+     * Pair Program 1
+     * File URL: http://agrigorenko.greenriverdev.com/328/pp1/
+     */
+
+    // Turn on error reporting -- this is critical!
+    ini_set('display_errors',1);
+    error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Program 1</title>
+</head>
+<body>
+    <h1>Pair Program 1</h1>
+
+</body>
+</html>

a687ae2de0085b7b5aadedf29017af3e5b029f9e
Alex Grigorenko <agrigorenko@mail.greenriver.edu>
2020-01-08 13:27:09 -0500

    Second step completed

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     // Turn on error reporting -- this is critical!
     ini_set('display_errors',1);
     error_reporting(E_ALL);
+
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +21,16 @@
 </head>
 <body>
     <h1>Pair Program 1</h1>
+
+    <?php
+        $numbers = array(7, 9, 8, 9, 8, 8, 6);
+        function printArr($numbers){
+            foreach ($numbers as $number){
+                echo $number.'<br>';
+            }
+        }
+        printArr($numbers);
+    ?>
 
 </body>
 </html>
